924d1abe0d97e4c515e64ad9b3d4cc92d27442c3
Hugh Brown <hbrown@amplify.com>
2013-10-15 12:26:05 -0400

    pep8 fix for Kerberos Requester

--- jenkinsapi/utils/krb_requester.py
+++ jenkinsapi/utils/krb_requester.py
@@ -5,6 +5,8 @@ from jenkinsapi.utils.requester import Requester
 from requests_kerberos import HTTPKerberosAuth, OPTIONAL
 
 #pylint: disable=W0222
+
+
 class KrbRequester(Requester):
     """
     A class which carries out HTTP requests with Kerberos/GSSAPI authentication.
